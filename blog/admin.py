02c8998584162a252a72ce9153d924b5fa3dab7a
elisa <elisa@hau-rock.de>
2023-03-18 00:35:00 +0100

    add database stuff

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Vendor, Supplement, Form, Unit, Active, Content, Shop, ShoppingItem
 
 admin.site.register(Post)
+admin.site.register(Vendor)
+
+
+class ContentInline(admin.TabularInline):
+    model = Content
+    extra = 0
+
+
+@admin.register(Supplement)
+class SupplementAdmin(admin.ModelAdmin):
+    inlines = (ContentInline, )
+    list_display = ("name", "pk", "amount", "vendor", "customBla", )
+    search_fields = ("name", )
+
+    def customBla(self, supplement):
+        return f"FormName: {supplement.form.name}"
+
+
+admin.site.register(Form)
+admin.site.register(Unit)
+admin.site.register(Active)
+admin.site.register(Content)
+admin.site.register(Shop)
+admin.site.register(ShoppingItem)
--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,70 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=2048)
+
+    def __str__(self):
+        return self.name
+
+
+class Form(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Active(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=2048)
+
+    def __str__(self):
+        return self.name
+
+
+class Supplement(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    amount = models.PositiveIntegerField()
+    url = models.URLField(max_length=2048)
+
+    def __str__(self):
+        return self.name
+
+
+class Content(models.Model):
+    supplement = models.ForeignKey(Supplement, on_delete=models.CASCADE)
+    active = models.ForeignKey(Active, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    dose = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.supplement.name}: {self.active.name} ({self.dose} {self.unit.name})"
+
+
+class ShoppingItem(models.Model):
+    supplement = models.ForeignKey(Supplement, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=19, decimal_places=2)
+
+    def __str__(self):
+        return self.price
