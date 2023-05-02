3af27dec2710e44dc16927a164c0b63bec8e9a71
elisa <elisa@hau-rock.de>
2023-05-02 21:47:05 +0200

    add database models

--- blog/models.py
+++ blog/models.py
@@ -18,9 +18,18 @@ class Form(models.Model):
         return self.name
 
 
+class Connection(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class ActiveIngredient(models.Model):
     name = models.CharField(max_length=200)
-    # interactons = many to many with self
+    connection = models.ManyToManyField(Connection, null=True, blank=True)
+    # interactons = many to many with self powered by chatGPT ;-)
+    interactions = models.ManyToManyField('self', symmetrical=False)
 
     def __str__(self):
         return self.name
@@ -28,17 +37,6 @@ class ActiveIngredient(models.Model):
 
 class Unit(models.Model):
     name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
-
-
-class Shop(models.Model):
-    name = models.CharField(max_length=200)
-    url = models.URLField(max_length=2048)
-    min_order_value_free_shipping = models.DecimalField(
-        max_digits=20, decimal_places=2)
-    shipping_price = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __str__(self):
         return self.name
@@ -52,6 +50,20 @@ class Ingredient(models.Model):
         return self.name
 
 
+class Trace(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class SpecialLabel(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Supplement(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     form = models.ForeignKey(Form, on_delete=models.CASCADE)
@@ -60,6 +72,8 @@ class Supplement(models.Model):
     active_ingredients = models.ManyToManyField(
         ActiveIngredient, through="Content", blank=True)
     ingredients = models.ManyToManyField(Ingredient, blank=True)
+    traces = models.ManyToManyField(Trace, blank=True)
+    special_labels = models.ManyToManyField(SpecialLabel, blank=True)
 
     def __str__(self):
         return self.name
@@ -76,6 +90,17 @@ class Content(models.Model):
         return f"{self.supplement.name}: {self.active_ingredient.name} ({self.dose} {self.unit.name})"
 
 
+class Shop(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=2048)
+    min_order_value_free_shipping = models.DecimalField(
+        max_digits=20, decimal_places=2)
+    shipping_price = models.DecimalField(max_digits=20, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
 class ShoppingItem(models.Model):
     supplement = models.ForeignKey(Supplement, on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
@@ -83,6 +108,30 @@ class ShoppingItem(models.Model):
 
     def __str__(self):
         return self.product_url
+
+
+class Discount(models.Model):
+    UNITS = (
+        ('shipping', 'Free Shipping'),
+        ('discount', 'Euro Price discount'),
+        ('2F1', 'Two for One'),
+    )
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    affiliate = models.BooleanField(default=False)
+    discount_code = models.CharField(max_length=200, blank=True)
+    discount_value = models.DecimalField(max_digits=19, decimal_places=2)
+    discount_unit = models.CharField(max_length=100, choices=UNITS)
+    valid_from = models.DateField(null=True, blank=True)
+    valid_until = models.DateField(null=True, blank=True)
+    min_order_value = models.DecimalField(
+        max_digits=19, decimal_places=2, null=True, blank=True)
+    discount_url = models.URLField(max_length=2048, null=True, blank=True)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    item = models.ManyToManyField(ShoppingItem, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Price(models.Model):
